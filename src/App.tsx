d1df9e690c6a6693c719221da2d6ee6c3b158b67
Alza Arh <alza.arhane97@gmail.com>
2024-11-17 17:43:54 +0330

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-medium text-center text-white my-8">
+        pomodoro
+      </h1>
+
+      <main className="max-w-[450px] mx-auto">
+        <ul className="bg-slate-900 rounded-full flex p-2 justify-between">
+          <li className="bg-red-400 rounded-full px-6 py-4 text-blue-950 font-medium">
+            pomodoro
+          </li>
+          <li className="rounded-full px-6 py-4 text-blue-950">short break</li>
+          <li className="rounded-full px-6 py-4 text-blue-950">long break</li>
+        </ul>
+      </main>
     </>
   );
 }
